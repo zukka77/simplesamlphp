7583fefbc201ef438d68cd20ee2396b52f49cf96
Tim van Dijen <tvdijen@gmail.com>
2021-07-11 18:37:59 +0200

    Fix php session handler

--- lib/SimpleSAML/SessionHandlerPHP.php
+++ lib/SimpleSAML/SessionHandlerPHP.php
@@ -138,20 +138,25 @@ class SessionHandlerPHP extends SessionHandler
      */
     public function newSessionId(): string
     {
-        // generate new (secure) session id
-        $sid_length = (int) ini_get('session.sid_length');
-        $sid_bits_per_char = (int) ini_get('session.sid_bits_per_character');
+        $sessionId = session_id();
 
-        if (($sid_length * $sid_bits_per_char) < 128) {
-            Logger::warning("Unsafe defaults used for sessionId generation!");
-        }
-        $sessionId = session_create_id();
+        if ($sessionId === false || $sessionId === '') {
+            // generate new (secure) session id
+            $sid_length = (int) ini_get('session.sid_length');
+            $sid_bits_per_char = (int) ini_get('session.sid_bits_per_character');
 
-        if (!$sessionId) {
-            Logger::warning("Secure session ID generation failed, falling back to custom ID generation.");
-            $sessionId = bin2hex(openssl_random_pseudo_bytes(16));
+            if (($sid_length * $sid_bits_per_char) < 128) {
+                Logger::warning("Unsafe defaults used for sessionId generation!");
+            }
+            $sessionId = session_create_id();
+
+            if (!$sessionId) {
+                Logger::warning("Secure session ID generation failed, falling back to custom ID generation.");
+                $sessionId = bin2hex(openssl_random_pseudo_bytes(16));
+            }
+            Session::createSession($sessionId);
         }
-        Session::createSession($sessionId);
+
         return $sessionId;
     }
 
